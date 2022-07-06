c3de92f42a23e55130b1fd477ad336cc32575af0
Abdülkerim AKSAK <abdulkerim.aksak@tubitak.gov.tr>
2022-07-06 10:17:44 +0300

    project init

---
+++ src/main/java/com/example/ExampleResource.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class ExampleResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("{value}")
+    public String hello(@PathParam("value") String value) {
+        return "Hello " + value.toUpperCase().concat("!");
+    }
+}
